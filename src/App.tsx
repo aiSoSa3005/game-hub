b00debb292b06a24213b338836ea01e2b8cd1944
aiSoSa3005 <omoruyiprosper05@gmail.com>
2025-08-22 22:14:36 +0200

    responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
-import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <>
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button className="bg-red-300 text-white">Click me</Button>
+      <div className="grid grid-cols-2 grid-rows-[auto_1fr] h-screen">
+        <nav className="col-span-2 bg-blue-500">Navbar</nav>
+        <aside className="hidden lg:bg-yellow-600 lg:block ">Aside</aside>
+        <main className="bg-orange-600 col-span-2 lg:col-span-1">Main</main>
       </div>
     </>
   );
